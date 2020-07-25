5b1df265b7f9158591bd8a5847a1250affad7dd4
Hiroaki Tomiyoshi <htomiyosi@gmail.com>
2020-07-26 01:23:17 +0900

    Finished tutorial `Creating Nested Layout Components` (https://www.gatsbyjs.org/tutorial/part-three/)

---
+++ src/utils/typography.js
@@ -0,0 +1,8 @@
+import Typograpy from "typography"
+import fairyGateTheme from "typography-theme-fairy-gates"
+import Typography from "typography"
+
+const typography = new Typograpy(fairyGateTheme)
+
+export const { scale, rhythm, option } = Typography
+export default typography
